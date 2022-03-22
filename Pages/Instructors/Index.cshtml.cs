167c97e59ae802e1400d125a436130cdadc16e26
Nhan Vo <nhoxben335@gmail.com>
2022-03-21 23:17:43 -0300

    Begin the phase 3

---
+++ Pages/Instructors/Index.cshtml.cs
@@ -0,0 +1,29 @@
+#nullable disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using NSCCCourseMap.Models;
+
+namespace nscccoursemap_nhoxben335.Pages.Instructors
+{
+    public class IndexModel : PageModel
+    {
+        private readonly NSCCCourseMapContext _context;
+
+        public IndexModel(NSCCCourseMapContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Instructor> Instructor { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Instructor = await _context.Instructors.ToListAsync();
+        }
+    }
+}
